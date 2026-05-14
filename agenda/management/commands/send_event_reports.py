bbd58e9d5c59a95c0a04ebd34f4c72a15141bba7
Éder Porto <eder.porto@hotmail.com>
2026-05-14 02:00:21 -0300

    messages and email

--- agenda/management/commands/send_event_reports.py
+++ agenda/management/commands/send_event_reports.py
@@ -1,12 +1,19 @@
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now
 from agenda.services import send_event_reports
+from datetime import datetime
 
 
 class Command(BaseCommand):
     help = "Send event reports to managers"
 
     def handle(self, *args, **options):
+        week = datetime.now().isocalendar().week
+
+        if week % 2 != 0:
+            self.stdout.write("Skipping this week")
+            return
+
         start = now()
         self.stdout.write("Starting report sending...")
         send_event_reports()
