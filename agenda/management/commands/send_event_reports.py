cd74794d3d23ae43ef5c0e6faf71754a124d8643
Éder Porto <eder.porto@hotmail.com>
2026-05-14 01:33:48 -0300

    messages and email

--- agenda/management/commands/send_event_reports.py
+++ agenda/management/commands/send_event_reports.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-
+from django.utils.timezone import now
 from agenda.services import send_event_reports
 
 
@@ -7,5 +7,13 @@ class Command(BaseCommand):
     help = "Send event reports to managers"
 
     def handle(self, *args, **options):
+        start = now()
+        self.stdout.write("Starting report sending...")
         send_event_reports()
+        end = now()
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Reports sent in {(end - start).total_seconds()} seconds"
+            )
+        )
         self.stdout.write(self.style.SUCCESS("Reports sent"))
